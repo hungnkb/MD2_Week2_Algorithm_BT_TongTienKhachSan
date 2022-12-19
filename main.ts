bb6458167de939b826bf117ab249f20a28c24f25
hungreo <hungnkb@gmail.com>
2022-12-20 01:56:17 +0700

    s

--- main.ts
+++ main.ts
@@ -1,28 +1,20 @@
 let arr = [[0, 1, 1, 2],
 [0, 5, 0, 0],
 [2, 0, 3, 3],]
+
 let totalCost = (data: number[][]): number => {
     let cost = 0;
-    let i = 0;
-    while (data) {
-        while (i < data.length) {
-            let j = 0;
-            while (j < data[i].length) {
-                if (data[i][j] != 0) {
-                    cost += data[i][j];
-                }
-                j++;
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+            if (data[i][j] != 0) {
+                cost += data[i][j];
             }
-            i++;
-            data.splice(i,1);
         }
-        
-        totalCost(data)
     }
-    return cost;
+    return cost
 }
 
 
-totalCost(arr)
+console.log(totalCost(arr))
 
 
