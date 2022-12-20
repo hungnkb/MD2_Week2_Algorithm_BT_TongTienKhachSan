e7932da39566a0989368d63df1d21401a102e5d2
unknown <hungnkb>
2022-12-20 17:28:27 +0700

    s

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
-let arr = [[0, 1, 1, 2],
-[0, 5, 0, 0],
-[2, 0, 3, 3],]
+let arr: any = [[0, 1, 1, 2],
+               [0, 5, 0, 0],
+               [2, 0, 3, 3],]
 
-let totalCost = (data: number[][]): number => {
-    let cost = 0;
-    for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < data[i].length; j++) {
-            if (data[i][j] != 0) {
-                cost += data[i][j];
+let totalCost = (data: number[][]): any => {
+    let totalCost = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length; j++) {
+            if (arr[i][j] == 0) {
+                arr[i+1][j] = 0;
             }
+            totalCost += arr[i][j]
         }
     }
-    return cost
+
+
 }
 
 
